7b23eabcbf3d4e6f7e9a835e4cdf1c8a374b5dc3
Lê Võ Hoàng Duy <toovataa@gmail.com>
2019-01-06 13:03:34 +0700

    code page admin

---
+++ Front-End/src/components/Staff/CreateAcc/CreateAcc.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./CreateAcc.css";
+
+class CreateAcc extends Component {
+  render() {
+    return (
+      <div className="staff-create-acc">
+        <div className="staff-create-header">
+          <h2>Tạo tài khoản khách hàng</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CreateAcc;
--- Front-End/src/components/Staff/Staff.jsx
+++ Front-End/src/components/Staff/Staff.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { NavLink, Route, Switch } from "react-router-dom";
 import "./Staff.css";
 import HeaderStaff from "./HeaderStaff";
+import CreateAcc from "./CreateAcc";
 
 class Staff extends Component {
   componentWillMount() {
@@ -14,12 +16,45 @@ class Staff extends Component {
           <div>
             <ul className="sidebar navbar-nav">
               <div className="scroll-box">
-                <div className="scroll-box-content" />
+                <div className="scroll-box-content">
+                  <NavLink
+                    to="/staff/create"
+                    className="nav-link-custom"
+                    activeClassName="nav-link-custom-active"
+                  >
+                    <div className="menu-staff-ele">
+                      <span className="menu-staff-title">
+                        <i className="far fa-address-card fa-2x" />
+                      </span>
+                      <span className="menu-staff-title">
+                        &nbsp;Tạo tài khoản khách hàng
+                      </span>
+                    </div>
+                  </NavLink>
+                  <NavLink
+                    to="/staff/user-manager"
+                    className="nav-link-custom"
+                    activeClassName="nav-link-custom-active"
+                  >
+                    <div className="menu-staff-ele">
+                      <span className="menu-staff-title">
+                        <i className="far fa-list-alt fa-2x" />
+                      </span>
+                      <span className="menu-staff-title">
+                        &nbsp;&nbsp;Quản lý khách hàng
+                      </span>
+                    </div>
+                  </NavLink>
+                </div>
               </div>
             </ul>
           </div>
           <div id="content-wrapper">
-            <div className="" />
+            <div className="content-wrapper-box">
+              <Switch>
+                <Route path="/staff/create" component={CreateAcc} />
+              </Switch>
+            </div>
           </div>
         </div>
       </div>
